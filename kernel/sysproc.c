baec51404dad6da255848e98701e2a64fbf20824
Aryan Saxena <aryansaxena1210@gmail.com>
2026-03-01 17:44:21 +0000

    add getprocinfo syscall and make it visible to user

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -12,7 +12,7 @@ sys_exit(void)
   int n;
   argint(0, &n);
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -43,7 +43,7 @@ sys_sbrk(void)
 
   argint(0, &n);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -57,8 +57,10 @@ sys_sleep(void)
   argint(0, &n);
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n)
+  {
+    if (killed(myproc()))
+    {
       release(&tickslock);
       return -1;
     }
@@ -88,4 +90,23 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64 sys_getprocinfo(void)
+{
+  struct proc *p = myproc();
+  struct proc_info info;
+  uint64 uaddr;
+
+  // Populate from the process control block
+  info.pid = p->pid;
+  info.ppid = p->parent ? p->parent->pid : 0;
+  info.state = p->state;
+  info.sz = p->sz;
+
+  argaddr(0, &uaddr);
+
+  if (copyout(p->pagetable, uaddr, (char *)&info, sizeof(info)) < 0)
+    return -1;
+  return 0;
 }
